f94dee5db8ff6c1fb21185a204aad75e41d9fa1c
Leftmy <leftmyfrid0708@gmail.com>
2026-09-21 02:00:45 +0300

    fix: another patch

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,6 @@ set -e
 mkdir -p /var/www/flaskapp
 cd /var/www/flaskapp
 
-python3 -m venv venv
+python3 -m venv venv || true
 ./venv/bin/pip install --upgrade pip
 ./venv/bin/pip install -r requirements.txt
